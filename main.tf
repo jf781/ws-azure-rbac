963f73f52e314b55a47c3256781969f46a1d6261
Joe Fecht <joe.fecht@ahead.com>
2023-01-03 12:45:07 -0600

    Updated module path

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #---------------------------------------------------------------
 
 module "rbac" {
-  source                      = "app.terraform.io/berlin-packaging-infra/module-rbac/azurerm"
+  source                      = "app.terraform.io/Joe-Demo/module-rbac/azurerm"
   # version                     = "0.1.1"
   built_in_role_assignments   = var.built_in_role_assignments
   custom_role_assignments     = var.custom_role_assignments
